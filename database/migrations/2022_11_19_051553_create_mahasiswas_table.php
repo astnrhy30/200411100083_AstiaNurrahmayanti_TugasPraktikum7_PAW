6c9b41b0713f5115eb4ec2ef8bd73a708dfc1bde
Astia Nurrahmayanti <83950535+astnrhy30@users.noreply.github.com>
2022-11-19 20:29:26 +0700

    Add files via upload

---
+++ database/migrations/2022_11_19_051553_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id('IDMhs_083', 11);
+            $table->string('NRP_083', 12);
+            $table->string('Nama_083', 256);
+            $table->string('Email_083', 100);
+            $table->string('Alamat_083', 256);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
